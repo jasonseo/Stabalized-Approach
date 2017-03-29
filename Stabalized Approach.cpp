1c7c9ed0f3878940af378aa652d1ed0c6fc47165
Jason Seo <jasonseo@usc.edu>
2017-03-29 01:37:58 -0700

    test

--- Stabalized Approach.cpp
+++ Stabalized Approach.cpp
@@ -4,17 +4,26 @@
 #include "stdafx.h"
 #include "Timer.cpp"
 
+
+int checkHeights() {
+
+}
+
+int checkVelocity() {
+
+}
+
+
 int main()
 {
 	Timer timer;
 	int height = 0;
 
-	while (true){
+	while (height != 0){
 		if (timer.getElapsed % 60 == 0) {
-
-		}
-		if (height == 0) {
-			break;
+			//check the descending fpm is between 700fpm and 1300fpm
+			//check velocity is not more than v_ref + 20kts (37.04km/h) 
+			//note: v_ref = v_o * 1.3, where v_o is the plane's maximum operating manuevering speed
 		}
 	}
 
